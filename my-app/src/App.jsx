d303a6c3d18948bf5a7bf3c9d8517d82bdf1655b
sssmirnov222 <121797442+sssmirnov222@users.noreply.github.com>
2023-06-13 20:10:49 +0300

    One

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import PopUp from './components/PopUp/PopUp';
+
+function App() {
+  return (
+    <div className="App">
+       <PopUp/>
+    </div>
+  );
+}
+
+export default App;
